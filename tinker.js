c102ad8ddac67c402f488485d664f8b3defd077d
Darragh Murphy <dmurphy@servuo.com>
2023-04-25 18:37:25 -0700

    Add ingredients for loop example

--- tinker.js
+++ tinker.js
@@ -1,11 +1,10 @@
-for (let num = 100; num <= 200; num++) {
-  if (num % 3 === 0 && num % 4 === 0) {
-    console.log("LoopyLighthouse");
-  } else if (num % 3 === 0) {
-    console.log("Loopy");
-  } else if (num % 4 === 0) {
-    console.log("Lighthouse");
-  } else {
-    console.log(num);
-  }
+let sum = 3 + 2;
+
+const doubleSum = function(a, b) {
+  let sum = a + b;
+  return sum * 2;
 }
+
+doubleSum(5, 4);
+
+console.log(sum);
